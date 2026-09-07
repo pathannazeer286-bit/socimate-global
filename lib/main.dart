073d50c7b5be40ab7cad9e46c17807432f9b3db4
pathannazeer286-bit <pathannazeer286@gmail.com>
2026-09-07 18:02:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,49 +19,13 @@ class SocialApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Social App MVP',
+      title: 'Socimate Global MVP',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const InitializationScreen(),
-    );
-  }
-}
-
-class InitializationScreen extends StatelessWidget {
-  const InitializationScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Day 1: Foundation'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Icon(
-              Icons.check_circle_outline,
-              size: 80,
-              color: Colors.green,
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Firebase Connected Successfully!',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Your 56-Day Master Project is ready.',
-              style: TextStyle(color: Colors.grey),
-            ),
-          ],
-        ),
-      ),
+      home: const LoginScreen(),
     );
   }
 }
